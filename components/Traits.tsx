6e34afbf6d30b94f425de4aa9b1d1785dca3bfca
quyenpq <quyenpq.work@gmail.com>
2025-11-18 04:52:58 +0700

    fix control settings fabric

--- components/Traits.tsx
+++ components/Traits.tsx
@@ -8,6 +8,15 @@ import useWindowSize from "@/hooks/useWindowSize";
 const deleteIcon =
   "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
 
+const CONTROL_SETTINGS = {
+  transparentCorners: false,
+  cornerColor: "blue",
+  cornerStyle: "rect",
+  cornerStrokeColor: "lightblue",
+  cornerSize: 10,
+  borderColor: "lightblue",
+};
+
 const Traits = () => {
   const canvasRef = useRef<Canvas | undefined>(undefined);
   const rectRef = useRef<Rect | undefined>(undefined);
@@ -127,6 +136,7 @@ const Traits = () => {
       clearExistingImages();
 
       const fabricImg = new FabricImage(imgElement, {
+        ...CONTROL_SETTINGS,
         left: 0,
         top: 0,
         selectable: false,
@@ -238,6 +248,7 @@ const Traits = () => {
     try {
       const imgElement = await loadHtmlImage(src);
       const avatarImage = new FabricImage(imgElement, {
+        ...CONTROL_SETTINGS,
         left: (canvas.getWidth() ?? 400) * 0.15,
         top: (canvas.getHeight() ?? 400) * 0.15,
       });
